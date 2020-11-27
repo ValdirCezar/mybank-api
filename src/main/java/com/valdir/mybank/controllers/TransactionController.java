9a16504b3f1501cf68e1e18f32eb386a786a74a8
Valdir Cezar <valdircezar312@gmail.com>
2020-11-27 11:42:10 -0300

    Swagger initiated and Controllers initiated

---
+++ src/main/java/com/valdir/mybank/controllers/TransactionController.java
@@ -0,0 +1,10 @@
+package com.valdir.mybank.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/transaction")
+public class TransactionController {
+
+}
